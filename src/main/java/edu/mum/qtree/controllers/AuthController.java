1953ec73449a03a7eac5f64e56954993f99c0d51
Omda <eshehata@mum.edu>
2019-09-16 00:47:03 -0700

    Swagger

--- src/main/java/edu/mum/qtree/controllers/AuthController.java
+++ src/main/java/edu/mum/qtree/controllers/AuthController.java
@@ -8,6 +8,8 @@ import edu.mum.qtree.models.entities.User;
 import edu.mum.qtree.models.entities.UserRole;
 import edu.mum.qtree.security.jwt.JwtTokenProvider;
 import edu.mum.qtree.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,6 +27,7 @@ import java.util.*;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@Api(value = "Authentication Resourse",description = "Authentication Resourse")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -41,6 +44,7 @@ public class AuthController {
     @Autowired
     UserRepository users;
 
+    @ApiOperation(value = "Make Login")
     @PermitAll
     @PostMapping("/signin")
     public ResponseEntity signin(@RequestBody AuthenticationRequestDto data) {
@@ -62,6 +66,7 @@ public class AuthController {
         }
     }
 
+    @ApiOperation(value = "Make User Registration")
     @PermitAll
     @PostMapping("/signup")
     public UserInfo signup(@RequestBody UserAddDto dto) {
